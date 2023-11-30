2389b7a51eca17c98405f3c3123d9f9418ac412d
aters <aters@gmail.com>
2023-12-01 02:03:39 +1100

    update dockerfile, workflow

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ require('dotenv').config();
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
